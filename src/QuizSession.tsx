1ccb9113a9867679ba7f6e75380155c0452e9698
letminjae <cmjj0824@naver.com>
2022-05-05 22:23:53 +0900

    FEAT : useReducer를 통한 단어문제 기능 구현

--- src/QuizSession.tsx
+++ src/QuizSession.tsx
@@ -1,36 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useReducer } from 'react'
 import { Link } from 'react-router-dom'
-
-// State
-type Quiz = {
-  index: number
-  text: string // 문제
-  answer: string // 정답
-  selections: string[] // 보기 목록 (정답 포함), 2지 선다
-}
-
-type QuizResult = {
-  quizIndex: number
-  createdAt: Date
-  answer: string // 정답
-  selected: string // 선택한 답
-  isCorrect: boolean // 정답여부
-}
-
-type State = {
-  isCompleted: boolean // computed
-  correctCount: number // computed
-  inCorrectCount: number // computed
-  currentIndex: number // computed
-  quizList: Quiz[]
-  quizResults: QuizResult[]
-}
+import { Quiz, QuizResult, State} from './types'
 
 // Action
-
-// Select 동작방식
-// 선지를 선택하면, 새로운 퀴즈결과가 생기고,
-// 다음 문제로 넘어가야 한다.
 type Select = {
   type: 'SELECT'
   payload: {
@@ -39,33 +11,108 @@ type Select = {
   }
 }
 
-type Action = Select
+type Action =
+  | Select
+  | {
+      type: 'CORRECT'
+      currentCount: number
+      currentIndex: number
+      payload: number
+    }
+  | {
+      type: 'INCORRECT'
+      correctCount: number
+      inCorrectCount: number
+    }
+  | {
+      type: 'COMPLETE'
+      isCompleted: boolean
+    }
+
+const initialData: State = {
+  isCompleted: false,
+  correctCount: 0,
+  inCorrectCount: 0,
+  currentIndex: 0,
+  payload: 1,
+  quizList: [
+    {
+      index: 0,
+      text: 'apple',
+      answer: 'n. 사과',
+      selections: ['n. 사과', 'n. 밀가루 반죽', 'a. 지속적인, 끈질긴']
+    },
+    {
+      index: 1,
+      text: 'brick',
+      answer: 'n. 벽돌',
+      selections: ['n. 벽돌', 'v. 뛰다, 급증하다', 'n. 사과']
+    },
+    {
+      index: 2,
+      text: 'leap',
+      answer: 'v. 뛰다, 급증하다',
+      selections: ['n. 벽돌', 'n. 완성, 성취', 'a. 지속적인, 끈질긴']
+    },
+    {
+      index: 3,
+      text: 'horn',
+      answer: 'n. 뿔, 경적',
+      selections: ['n. 관광, 관광 사업', 'n. 밀가루 반죽', 'n. 뿔, 경적']
+    }
+  ],
+  quizResults: []
+}
 
 function quizSessionReducer(state: State, action: Action) {
-  // TODO
-  // 선택한 선지에 따라
-  // state 값이 변경되어야 함.
-  // 예를 들어, 퀴즈 결과가 생성되고
-  // 맞은 혹은 틀린 개수가 업데이트 되고,
-  // 다음 퀴즈로 넘어가야 함.
-  const newState = { ...state }
-  return newState
+  switch (action.type) {
+    case 'CORRECT':
+      return {
+        ...state,
+        currentIndex: state.currentIndex++,
+        correctCount: state.correctCount++
+      }
+    case 'INCORRECT':
+      return {
+        ...state,
+        currentIndex: state.currentIndex++,
+        inCorrectCount: state.inCorrectCount++
+      }
+    case 'COMPLETE':
+      return {
+        ...state,
+        isCompleted: true
+      }
+    default:
+      throw new Error('Error : Cannot read Action')
+  }
 }
 
 // View
-function QuizSessionView(state: State, onClick: (selected: string) => void) {
+function QuizSessionView(selector: State, onClick: (selected: string) => void, dispatch: Function) {
   function QuizView(quiz: Quiz) {
     const articleStyle = {
       marginTop: '16px',
       padding: '8px',
       background: '#efefef'
     }
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+      const htmlTarget = e.target as HTMLElement
+      if (htmlTarget.innerText === quiz.answer) {
+        dispatch({ type: 'CORRECT' })
+      } else {
+        dispatch({ type: 'INCORRECT' })
+      }
+      if (selector.quizList.length - 1 === selector.currentIndex) {
+        dispatch({ type: 'COMPLETE' })
+      }
+    }
     return (
       <article style={articleStyle}>
         <header>{quiz.text}</header>
         {quiz.selections.map((sel, idx) => {
           return (
-            <button key={idx} onClick={() => onClick(sel)}>
+            <button key={idx} onClick={handleClick}>
               {sel}
             </button>
           )
@@ -74,19 +121,20 @@ function QuizSessionView(state: State, onClick: (selected: string) => void) {
     )
   }
 
-  const currentQuiz = state.quizList[state.currentIndex]
+  const currentQuiz = selector.quizList[selector.currentIndex]
 
   return (
     <section>
-      <div>완료 여부: {state.isCompleted ? '완료' : '미완료'}</div>
-      <div>맞은 개수 {state.correctCount}</div>
-      <div>틀린 개수 {state.inCorrectCount}</div>
-      {state.isCompleted ? <Link to='/'>홈으로</Link> : QuizView(currentQuiz)}
+      <div>완료 여부: {selector.isCompleted ? '완료' : '미완료'}</div>
+      <div>맞은 개수 {selector.correctCount}</div>
+      <div>틀린 개수 {selector.inCorrectCount}</div>
+      {selector.isCompleted ? <Link to='/'>홈으로</Link> : QuizView(currentQuiz)}
     </section>
   )
 }
 
 function QuizSession() {
+  const [selector, dispatch] = useReducer(quizSessionReducer, initialData)
   const [initalLoaded, setInitalLoaded] = useState(false)
   const [state, setState] = useState<State | null>(null)
 
@@ -94,48 +142,6 @@ function QuizSession() {
     // 임시로 1초간 타임 아웃.
     await new Promise((resolve) => setTimeout(resolve, 1000))
 
-    const initialData = [
-      {
-        text: 'apple',
-        meaning: 'a. 사과'
-      },
-      {
-        text: 'brick',
-        meaning: 'n. 벽돌'
-      },
-      {
-        text: 'completion',
-        meaning: 'n. 완성, 성취'
-      },
-      {
-        text: 'obstacle',
-        meaning: 'n. 장애물'
-      },
-      {
-        text: 'horn',
-        meaning: 'n. 뿔, 경적'
-      },
-      {
-        text: 'dough',
-        meaning: 'n. 밀가루 반죽'
-      },
-      {
-        text: 'leap',
-        meaning: 'v. 뛰다, 급증하다.'
-      },
-      {
-        text: 'pearl',
-        meaning: 'n. 진주, 진주색'
-      },
-      {
-        text: 'tourism',
-        meaning: 'n. 관광, 관광 사업'
-      },
-      {
-        text: 'persisent',
-        meaning: 'a. 지속적인, 끈질긴'
-      }
-    ]
     // TODO
     // initialData를 State 타입으로 변경 후 리턴한다.
     // quizList[].selections 을 만드는 조건은
@@ -147,20 +153,8 @@ function QuizSession() {
       correctCount: 0,
       inCorrectCount: 0,
       currentIndex: 0,
-      quizList: [
-        {
-          index: 0,
-          text: 'apple',
-          answer: 'n. 사과',
-          selections: ['n. 사과', 'n. 밀가루 반죽']
-        },
-        {
-          index: 1,
-          text: 'brick',
-          answer: 'n. 벽돌',
-          selections: ['n. 벽돌', 'v. 뛰다, 급증하다']
-        }
-      ],
+      payload: 1,
+      quizList: [],
       quizResults: []
     }
   }
@@ -169,27 +163,20 @@ function QuizSession() {
     ;(async () => {
       // 초기 데이터 불러오기
       if (!initalLoaded) {
-        const initalState = await initState()
-        setState(initalState)
+        const initialState = await initState()
+        setState(initialState)
         setInitalLoaded(true)
       }
     })()
   }, [initalLoaded])
 
-  const quizSelected = (selected: string) => {
+  const quizSelected = () => {
     if (state == null) return
 
-    const newState = quizSessionReducer(state, {
-      type: 'SELECT',
-      payload: {
-        quizIndex: state.currentIndex,
-        selected: selected
-      }
-    })
-    setState(newState)
+    return setState(selector)
   }
 
-  return <div>{state ? QuizSessionView(state, quizSelected) : '로딩중...'}</div>
+  return <div>{state ? QuizSessionView(selector, quizSelected, dispatch) : '로딩중...'}</div>
 }
 
 export default QuizSession
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,29 @@
 export interface Word {
-    text: string
-    meaning: string
-  }
+  text: string
+  meaning: string
+}
+
+export type Quiz = {
+  index: number
+  text: string
+  answer: string
+  selections: string[]
+}
+
+export type QuizResult = {
+  quizIndex: number
+  createdAt: Date
+  answer: string
+  selected: string
+  isCorrect: boolean
+}
+
+export type State = {
+  isCompleted: boolean // computed
+  correctCount: number // computed
+  inCorrectCount: number // computed
+  currentIndex: number // computed
+  quizList: Quiz[]
+  quizResults: QuizResult[]
+  payload: number
+}
